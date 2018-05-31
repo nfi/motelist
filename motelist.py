3a0cfd57a52a3d29da2a5706dd8fc20a6cf287c5
George Oikonomou <cexgo@it035769.local>
2018-05-31 16:59:57 +0100

    Tidy-up license, copyright and authors

--- motelist.py
+++ motelist.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2013, Janis Judvaitis
+# Copyright (c) 2018, University of Bristol <www.bristol.ac.uk>
+# All rights reserved.
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
+#
 #  * Redistributions of source code must retain the above copyright notice,
 #    this list of  conditions and the following disclaimer.
 #  * Redistributions in binary form must reproduce the above copyright
-#   notice, this list of conditions and the following disclaimer in the
-#   documentation and/or other materials provided with the distribution.
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -18,11 +24,11 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 #
-# Author: Janis Judvaitis, 2013
-#
-
+# Authors:
+#   Janis Judvaitis
+#   Atis Elsts
+#   George Oikonomou
 import os, sys, threading, time, serial
 import importlib
 
