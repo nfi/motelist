295abeadcbe1f1a288839985649c01d95cdcf1b9
George Oikonomou <g.oikonomou@bristol.ac.uk>
2018-06-03 12:51:16 +0100

    Adjust OSX backend to new API

--- backends/osx.py
+++ backends/osx.py
@@ -35,19 +35,15 @@ import xml.dom.minidom as dom
 import backends.backend
 
 
-tmp_file = '/tmp/get_ports_osx.xml'
-
-port_patterns = [
-    '/dev/tty.SLAB*',
-    '/dev/tty.usbmodem*1',
-]
-
-
 class Backend(backends.backend.Backend):
     os = 'darwin'
+    tmp_file = '/tmp/get-ports-osx.xml'
 
+    port_patterns = [
+        '/dev/tty.SLAB*',
+        '/dev/tty.usbmodem*1',
+    ]
 
-class Device(object):
     search_attrs = {
         'idVendor': 'vid',
         'USB Vendor Name': 'vendor',
@@ -56,103 +52,80 @@ class Device(object):
         'USB Serial Number': 'serial'
     }
 
-    @classmethod
-    def get_search_attrs(cls):
-        return cls.search_attrs.keys()
+    def __init__(self, motelist):
+        self.__motelist = motelist
 
-    def __init__(self, path, dom_node):
-        self.path = path
-        self.dom_node = dom_node
-        self.pid = 0x00
-        self.vid = 0x00
-        self.vendor = 'unknown'
-        self.product = 'unknown'
-        self.serial = 'unknown'
-
-    def set(self, key, val):
+    @staticmethod
+    def get_dom_node_text(node):
+        """Returns the text inside a DOM node, if one exists. None otherwise"""
         try:
-            setattr(self, self.search_attrs[key], int(val))
+            if node.firstChild.nodeType == dom.Node.TEXT_NODE:
+                return node.firstChild.data
+        except AttributeError:
+            return None
 
-        except ValueError:
-            setattr(self, self.search_attrs[key], val)
+    def dom_node_to_mote(self, port, dom_node):
+        mote = self.__motelist.create_mote()
+        mote.port = port
+        parent = dom_node.parentNode.parentNode.parentNode.parentNode.parentNode
 
-    def as_list(self):
-        return [
-            self.path,
-            self.product,
-            ' '.join(('VID=0x%04X' % self.vid,
-                      'PID=0x%04X' % self.pid,
-                      'Serial=%s' % self.serial,
-                      'Vendor=%s' % self.vendor))
-        ]
-
-
-def node_text_cmp(node, text_iter):
-    """Inspects the text inside a node. Returns the text itself if it appears
-    inside text_iter. None otherwise"""
-    try:
-        if node.firstChild.nodeType == dom.Node.TEXT_NODE:
-            s = node.firstChild.data
-            return s if s in text_iter else None
-    except AttributeError:
-        return None
-
-
-def read_iokit():
-    ioreg_cmd = '/usr/sbin/ioreg -p IOService -k IODialinDevice -l -r -t -a'
-    try:
-        outfile = open(tmp_file, 'w')
-        subprocess.call(ioreg_cmd, shell=True, stdout=outfile)
-        document = dom.parse(tmp_file)
-        return document
-    except IOError:
-        raise
-
-
-def comports():
-    ports = []
-    devices = []
-
-    for p in port_patterns:
-        ports.extend(glob.glob(p))
-
-    if len(ports) == 0:
-        return ""
-
-    # Invoke ioreg. This will give us an XML file of connected devices of a
-    # specific class. We will then extract information from that XML file.
-    doc = read_iokit()
-
-    # Retrieve all XML <string>. One of them will contain the device port
-    strings = doc.getElementsByTagName('string')
-
-    # Inspect all those <string> elements to see if they have a data value that
-    # also exists in the list of identified ports. For those elements, traverse
-    # the DOM upwards and store their grand-grand-grand-grand-parent...
-    # This will be the <dict> element that appears when a device gets connected
-    for s in strings:
-        text = node_text_cmp(s, ports)
-        if text is not None:
-            parent = s.parentNode.parentNode.parentNode.parentNode.parentNode
-            devices.append(Device(text, parent))
-            ports.remove(text)
-
-        # Stop searching if we have found a match for all identified ports
-        if len(ports) == 0:
-            break
-
-    # Traverse the list of those <dict> elements. For each element, search all
-    # its children using depth=1. Collect and store relevant info.
-    for device in devices:
-        child = device.dom_node.firstChild
+        # For a given DOM node, search all its children using depth=1. Collect
+        # relevant info and populate the respective Mote object.
+        child = parent.firstChild
         while child is not None:
             if child.nodeType == dom.Node.ELEMENT_NODE:
                 if child.tagName == 'key':
-                    child_text = node_text_cmp(child, Device.get_search_attrs())
-                    if child_text is not None:
-                        device.set(
-                            child_text,
-                            child.nextSibling.nextSibling.firstChild.data)
+                    child_text = self.get_dom_node_text(child)
+                    if child_text in self.search_attrs.keys():
+                        val = self.get_dom_node_text(
+                            child.nextSibling.nextSibling)
+                        try:
+                            setattr(mote, self.search_attrs[child_text],
+                                    int(val))
+
+                        except ValueError:
+                            setattr(mote, self.search_attrs[child_text], val)
+
             child = child.nextSibling
 
-    return [d.as_list() for d in devices]
+    @classmethod
+    def read_iokit(cls):
+        ioreg_cmd = '/usr/sbin/ioreg -p IOService -k IODialinDevice -l -r -t -a'
+        try:
+            outfile = open(cls.tmp_file, 'w')
+            subprocess.call(ioreg_cmd, shell=True, stdout=outfile)
+            document = dom.parse(cls.tmp_file)
+            return document
+        except IOError:
+            raise
+
+    def run(self):
+        ports = []
+
+        for p in self.port_patterns:
+            ports.extend(glob.glob(p))
+
+        if len(ports) == 0:
+            return None
+
+        # Invoke ioreg. This will give us an XML file of connected devices of a
+        # specific class. We will then extract information from that XML file.
+        doc = self.read_iokit()
+
+        # Retrieve all XML <string>. One of them will contain the device port
+        string_elemenets = doc.getElementsByTagName('string')
+
+        # Inspect all those <string> elements to see if they have a data value
+        # that also exists in the list of identified ports. For those elements,
+        # traverse the DOM upwards and store their grand-grand-grand-parent...
+        # This will be the <dict> element that appears when a device gets
+        # connected.
+        for se in string_elemenets:
+            port = self.get_dom_node_text(se)
+            if port in ports:
+                self.dom_node_to_mote(port, se)
+                ports.remove(port)
+
+            # Stop searching if we have found a match for all identified ports
+            if len(ports) == 0:
+                break
