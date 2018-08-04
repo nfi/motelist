c57628de7a5f0347cf41876f24db8f5666cb247c
George Oikonomou <g.oikonomou@bristol.ac.uk>
2018-08-04 02:15:41 +0100

    Linux backend work

--- backends/linux.py
+++ backends/linux.py
@@ -29,139 +29,81 @@
 #   George Oikonomou <g.oikonomou@bristol.ac.uk>
 import os
 import glob
-import re
 import backends.backend
 
 
-class Backend(backends.backend.Backend):
+class Device(object):
+    attr_file_map = {
+        'vid': 'idVendor',
+        'pid': 'idProduct',
+        'product': 'product',
+        'vendor': 'manufacturer',
+        'serial': 'serial'
+    }
+
+    @classmethod
+    def detect(cls, port, motelist, sys_usb):
+        base = os.path.basename(port)
+
+        mote = motelist.create_mote()
+        mote.port = port
+
+        for attr, file in cls.attr_file_map.items():
+            value = cls.__read_line(os.path.join(sys_usb, file))
+            try:
+                setattr(mote, attr, '0x%04X' % (int(value, 16),))
+            except ValueError:
+                setattr(mote, attr, value)
+
+    @classmethod
+    def __read_line(cls, filename):
+        line = "Unknown"
+        try:
+            with open(filename) as f:
+                line = f.readline().strip()
+        finally:
+            return line
+
+
+class USBSerialDevice(object):
+    @staticmethod
+    def visit(port, motelist):
+        base = os.path.basename(port)
+
+        sys_dev_path = '/sys/class/tty/{}/device/driver/{}'.format(base, base)
+
+        if os.path.exists(sys_dev_path):
+            sys_usb = os.path.dirname(
+                os.path.dirname(os.path.realpath(sys_dev_path)))
+
+
+class CDCACMDevice(object):
+    @staticmethod
+    def visit(port, motelist):
+        base = os.path.basename(port)
+
+        sys_dev_path = '/sys/class/tty/{}/device'.format(base, )
+
+        if os.path.exists(sys_dev_path):
+            sys_usb = os.path.dirname(os.path.realpath(sys_dev_path))
+
+
+class LinuxBackend(backends.backend.Backend):
     os = 'linux'
 
+    port_patterns = [
+        '/dev/ttyUSB*',
+        '/dev/ttyACM*',
+    ]
 
-printVID = False
+    def run(self):
+        ports = []
 
-def read_line(filename):
-    """help function to read a single line from a file. returns none"""
-    line = "Unknown"
-    try:
-        with open(filename) as f:
-            line = f.readline().strip()
-    finally:
-        return line
+        for p in self.port_patterns:
+            ports.extend(sorted(glob.glob(p)))
 
-def re_group(regexp, text):
-    """search for regexp in text, return 1st group on match"""
-    m = re.search(regexp, text)
-    if m: return m.group(1)
+        for port in ports:
+            USBSerialDevice.visit(port, self.__motelist)
+            CDCACMDevice.visit(port, self.__motelist)
 
 
-# try to extract descriptions from sysfs. this was done by experimenting,
-# no guarantee that it works for all devices or in the future...
-
-def usb_sysfs_hw_string(sysfs_path):
-    """given a path to a usb device in sysfs, return a string describing it"""
-    snr = read_line(sysfs_path + '/serial')
-    if snr:
-        snr_txt = '%s' % (snr,)
-    else:
-        snr_txt = ''
-    if printVID:
-        return 'USB VID:PID=%s:%s SNR=%s' % (
-                read_line(sysfs_path + '/idVendor'),
-                read_line(sysfs_path + '/idProduct'),
-                snr_txt
-                )
-    else:
-        return snr_txt
-
-# Old and wrong
-# def usb_lsusb_string(sysfs_path):
-#    bus, dev = os.path.basename(os.path.realpath(sysfs_path)).split('-')
-#    print bus, " :::: ", dev
-#    try:
-#        desc = popen(['lsusb', '-v', '-s', '%s:%s' % (bus, dev)])
-#        # descriptions from device
-#        iManufacturer = re_group('iManufacturer\s+\w+ (.+)', desc)
-#        iProduct = re_group('iProduct\s+\w+ (.+)', desc)
-#        iSerial = re_group('iSerial\s+\w+ (.+)', desc) or ''
-#        # descriptions from kernel
-#        idVendor = re_group('idVendor\s+0x\w+ (.+)', desc)
-#        idProduct = re_group('idProduct\s+0x\w+ (.+)', desc)
-#        print "####", iManufacturer, iProduct, iSerial, idVendor, idProduct
-#        # create descriptions. prefer text from device, fall back to the others
-#        return '%s %s %s' % (iManufacturer or idVendor, iProduct or idProduct, iSerial)
-#    except IOError:
-#        return "IOError"
-
-def usb_string(sysfs_path):
-    # Get dir name in /sys/bus/usb/drivers/usb for current usb dev
-    dev = os.path.basename(os.path.realpath(sysfs_path))
-    devDir = os.path.join("/sys/bus/usb/drivers/usb", dev)
-
-    try:
-        # Go to usb dev directory
-        product = read_line(os.path.join(devDir, "product"))
-        manufacturer = read_line(os.path.join(devDir, "manufacturer"))
-        result = product + " by " + manufacturer
-    except:
-        result = "Unknown device"
-
-    return result
-
-def describe(device):
-    """
-    Get a human readable description.
-    For USB-Serial devices try to run lsusb to get a human readable description.
-    For USB-CDC devices read the description from sysfs.
-    """
-    base = os.path.basename(device)
-    # USB-Serial devices
-    sys_dev_path = '/sys/class/tty/%s/device/driver/%s' % (base, base)
-    if os.path.exists(sys_dev_path):
-        sys_usb = os.path.dirname(os.path.dirname(os.path.realpath(sys_dev_path)))
-        return usb_string(sys_usb)
-
-    # Arduino wants special handling
-    sys_dev_path = '/sys/class/tty/%s/device/driver/' % (base)
-    if os.path.exists(sys_dev_path):
-        for x in os.listdir(sys_dev_path):
-            # Driver directory's name contains device ID in /sys/bus/usb/drivers/usb
-            temp = x.split(":")
-            if len(temp) == 2:
-                # No Arduino adds, need to save space!
-                return usb_string(temp[0]).replace("(www.arduino.cc)", "").strip()
-
-    # USB-CDC devices
-    sys_dev_path = '/sys/class/tty/%s/device/interface' % (base,)
-    if os.path.exists(sys_dev_path):
-        return read_line(sys_dev_path)
-
-    return base
-
-def hwinfo(device):
-    """Try to get a HW identification using sysfs"""
-    base = os.path.basename(device)
-    if os.path.exists('/sys/class/tty/%s/device' % (base,)):
-        # PCI based devices
-        sys_id_path = '/sys/class/tty/%s/device/id' % (base,)
-        if os.path.exists(sys_id_path):
-            return read_line(sys_id_path), sys_id_path
-        # USB-Serial devices
-        sys_dev_path = '/sys/class/tty/%s/device/driver/%s' % (base, base)
-        if os.path.exists(sys_dev_path):
-            sys_usb = os.path.dirname(os.path.dirname(os.path.realpath(sys_dev_path)))
-            return usb_sysfs_hw_string(sys_usb), sys_usb
-        # USB-CDC devices
-        if base.startswith('ttyACM'):
-            sys_dev_path = '/sys/class/tty/%s/device' % (base,)
-            if os.path.exists(sys_dev_path):
-                path = sys_dev_path + '/..'
-                return usb_sysfs_hw_string(path), path
-    return 'n/a', ''  # XXX directly remove these from the list?
-
-def comports():
-    devices = glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*')
-    result = []
-    for d in devices:
-        serial, path = hwinfo(d)
-        result.append((d, describe(d), serial, path))
-    return result
