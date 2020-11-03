3fb552ea3acf10b9d935ce7988adad9dec63b63a
George Oikonomou <>
2020-11-03 20:47:06 +0000

    Stop searching for '/dev/tty.usbserial* on macOS
    Searching for ports named with this pattern was added in c6b314d for reasons that are not really good enough. This commit stops searching for this pattern, since most of those ports are not actually usable serial interfaces.

--- backends/osx.py
+++ backends/osx.py
@@ -43,7 +43,6 @@ class OSXBackend(backends.backend.Backend):
     port_patterns = [
         '/dev/tty.SLAB*',
         '/dev/tty.usbmodem*',
-        '/dev/tty.usbserial*',
     ]
 
     search_attrs = {
